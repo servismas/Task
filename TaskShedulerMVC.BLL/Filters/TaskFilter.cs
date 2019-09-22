e183114dd0cba5fae76515a5402a2c4e53303f29
servismas <servismas@gmail.com>
2019-09-22 16:52:28 +0300

    Added Filter, TaskFilter

---
+++ TaskShedulerMVC.BLL/Filters/TaskFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskShedulerMVC.BLL.Filters
+{
+    public class TaskFilter
+    {
+        string Name { get; set; }
+        public Expression<Func<Task, bool>> Expression { get; set; }
+    }
+}
